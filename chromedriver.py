6e9847720832f2103354551150e82aa2af41c7de
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-07-09 12:23:02 +0200

    feat: update chromedriver.py to use headless mode for Chrome >= 109

--- chromedriver.py
+++ chromedriver.py
@@ -1,5 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless=new") # for Chrome >= 109
+# chrome_options.add_argument("--headless")
+# chrome_options.headless = True # also works
+driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome()
 
 url = "https://google.com/"
-driver = webdriver.Chrome()
 driver.get(url)
